42110769bd0d287cb629fae44c4989a31c146f81
MyDXD <dislove107@gmail.com>
2024-11-12 09:22:47 +0700

    ปักมุด google map

--- contactus.php
+++ contactus.php
@@ -3,7 +3,18 @@ include "nav_bar.php";
 ?>
 
 <div class="mx-auto bg-gray-100 rounded-lg shadow-md mt-2 max-w-2xl px-4 py-16 sm:px-6 sm:py-16 lg:max-w-7xl lg:px-8">
+    <!-- <h1 class="text-center text-3xl font-semibold text-gray-800 mb-8">Contact Us</h1> -->
 
-<h1 class="text-center">contactus</h1>
+    <div class="relative rounded-lg overflow-hidden shadow-lg">
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10658.807727220928!2d100.43036974137557!3d7.0296673501576725!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x304d26260cc89225%3A0x2f029c6eb6a80cd8!2sBan%20Khok%20Mao%20School!5e1!3m2!1sen!2sth!4v1731342621741!5m2!1sen!2sth"
+            class="w-full h-96 border-1 border-gray-300 rounded-lg" frameborder="0" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+        </iframe>
+    </div>
 
+    <p class="mt-8 text-center text-gray-600">
+        
+    </p>
+
+เด็กโคกเมานิ้พี่บ่าว
 </div>
